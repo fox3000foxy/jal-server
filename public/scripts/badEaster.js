4986ce503ca78f975aea1257cb4fd1908eaf4bdf
Fox3000 Foxy <40730498+fox3000foxy@users.noreply.github.com>
2022-01-24 12:48:25 +0000

    Add an easter egg and rename characters folders

---
+++ public/scripts/badEaster.js
@@ -0,0 +1,37 @@
+keypressHistory = ['','','','','','','','']
+bad = null
+window.addEventListener('keydown',(ev)=>{
+	for(i=1;i<8;i++) {
+		keypressHistory[i-1] = keypressHistory[i]
+	}
+	keypressHistory[7] = ev.key
+	konamiCodeCheck()
+})
+
+function konamiCodeCheck() {
+	if(
+		keypressHistory[0] == "ArrowUp" &&
+		keypressHistory[1] == "ArrowUp" &&
+		keypressHistory[2] == "ArrowDown" &&
+		keypressHistory[3] == "ArrowDown" &&
+		keypressHistory[4] == "ArrowLeft" &&
+		keypressHistory[5] == "ArrowRight" &&
+		keypressHistory[6] == "ArrowLeft" &&
+		keypressHistory[7] == "ArrowRight"
+	)
+	konami()
+}
+
+function konami() {
+	if(!bad) {
+		bad = document.createElement('audio')
+		bad.setAttribute('autoplay','')
+		bad.src = '/musics/badEaster.mp3'
+		bad.currentTime = 2.65
+		document.body.appendChild(bad)
+	}
+	else {
+		bad.remove()
+		bad = null
+	}
+}
